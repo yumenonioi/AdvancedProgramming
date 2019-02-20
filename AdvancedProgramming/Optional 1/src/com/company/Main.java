ce03e4855dbec3b9708a6c4d331748be9db1992c
cristian98st <44675214+cristian98st@users.noreply.github.com>
2019-02-20 22:57:31 +0200

    Upload1

---
+++ AdvancedProgramming/Optional 1/src/com/company/Main.java
@@ -0,0 +1,77 @@
+package com.company;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        int [][] matrix = new int[20][20];
+        int m = 0;
+
+        if(args.length != 2) throw new IllegalArgumentException("2 parameters required");
+
+        try{
+            int n = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.println("First argument is not integer");
+        }
+
+        int n = Integer.parseInt(args[0]);
+
+        if(n%2 == 0) throw new IllegalArgumentException("Number is not odd.");
+
+        if(args[1].equals("complete")) {
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=n;j++)
+                {
+                    if(i==j) matrix[i][j]=0;
+                    else {
+                        matrix[i][j]=1;
+                        m++;
+                    }
+                }
+            }
+        }
+
+        else if(args[1].equals("cycle")) {
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=n;j++)
+                {
+                    if(i==1 && j==n) {
+                        matrix[i][j]=1;
+                        m++;
+                    }
+                    else if(j==1 && i==n) {
+                        matrix[i][j]=1;
+                        m++;
+                    }
+                    else if(Math.abs(i-j) == 1) {
+                        matrix[i][j]=1;
+                        m++;
+                    }
+                    else matrix[i][j]=0;
+                }
+            }
+        }
+
+        else if(args[1].equals("random")) {
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=n;j++)
+                {
+                    int element = (int) (Math.random() * 2);
+                    matrix[i][j] = element;
+                    if(element == 1) m++;
+                }
+            }
+        }
+
+        else throw new IllegalArgumentException("The type of graph is not valid.");
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++)
+                System.out.print(matrix[i][j] + " ");
+            System.out.print("\n");
+        }
+
+        System.out.println("Number of edges is:" + m/2);
+    }
+}
